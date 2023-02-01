8aded9ff8db0ec8e0919d7428197d6e8c726bf99
NawafLawati <nawaf.lawati@gmail.com>
2023-02-01 12:09:44 +0400

    Changed Splash, added Home Screen elements initial UI, fixed up onboarding

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,9 +10,115 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-          child: Text("Under construction soon!"),
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Card(
+                color: Colors.deepPurple,
+                child: Column(
+                  children: const <Widget>[
+                    Text("Mortgage Calculator"),
+                    SizedBox(height: 20),
+                    Text("Loan amount"),
+                    SizedBox(height: 20),
+                    Text("Down Payment"),
+                    SizedBox(height: 20),
+                    Text("Length of loan")
+                  ],
+                ),
+                elevation: 10,
+              ),
+              Row(
+                children: const [
+                  Text("Offers"),
+                  Icon(Icons.compare_arrows),
+                  Icon(Icons.sort),
+                ],
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    const Text("Sohar International Image here"),
+                    Row(
+                      children: <Widget>[
+                        Column(
+                          children: const <Widget>[
+                            Text("3.91%"),
+                            Text("Interest Rate"),
+                          ],
+                        ),
+                        Column(
+                          children: const <Widget>[
+                            Text("350"),
+                            Text("Monthly Payment"),
+                          ],
+                        ),
+                        Column(
+                          children: const <Widget>[
+                            Text("450"),
+                            Text("Min. salary"),
+                          ],
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: const <Widget>[
+                        Text("Learn more"),
+                        Icon(Icons.keyboard_arrow_down),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          const ButtonBar(
+            children: [
+              Text("Apply for a mortgage")
+            ],
+          ),
+        ],
+      ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          bottomOpacity: 1.0,
+          elevation: 0.5,
+          title: const Text(
+            "Dari",
+            style: TextStyle(
+                fontFamily: 'Nunito',
+                fontWeight:  FontWeight.bold,
+                fontSize: 30.0
+            ),
+          ),
+          centerTitle: true,
+          foregroundColor: const Color(0xFF665BEF),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.calculate),
+                label: 'Calculator',
+                backgroundColor: Colors.white
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.content_paste_search),
+                label: 'Track Application',
+                backgroundColor: Colors.white
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+              backgroundColor: Colors.white,
+            ),
+          ],
+          type: BottomNavigationBarType.shifting,
+          selectedItemColor: Colors.lightBlueAccent,
+          iconSize: 40,
+          elevation: 10
       ),
     );
   }
--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:dari_replica/home_screen.dart';
+import 'package:dari_replica/page_indicator.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -11,100 +12,116 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
+    final PageController controller = PageController();
     return Scaffold(
       backgroundColor: Colors.white,
-      body: PageView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            children:  <Widget>[
-              const SizedBox(height: 40),
-              Image.asset('assets/onboarding_image_1.png'),
-              const SizedBox(height: 40),
-              const Text(
-                  "Get Pre-Approved in 15 minutes!",
-                  style: TextStyle(
-                      fontFamily: 'Nunito',
-                      fontWeight:  FontWeight.bold,
-                      fontSize: 30.0
-                  ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                  "Dari allows you to submit your pre-approval online and within 15 mins you will get pre-approved",
-                style: TextStyle(
-                    fontFamily: 'Nunito',
-                    color: Colors.grey,
-                    fontSize: 12.0,
+      body: Stack(
+        children: [
+          PageView(
+            controller: controller,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  children:  <Widget>[
+                    const SizedBox(height: 40),
+                    Image.asset('assets/onboarding_image_1.png'),
+                    const SizedBox(height: 40),
+                    const Text(
+                      "Get pre-approved in 15 minutes!",
+                      style: TextStyle(
+                          fontFamily: 'Nunito',
+                          fontWeight:  FontWeight.bold,
+                          fontSize: 30.0
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Dari allows you to submit your pre-approval online and within 15 mins you will get pre-approved",
+                      style: TextStyle(
+                        fontFamily: 'Nunito',
+                        color: Colors.grey,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
                 ),
-                textAlign: TextAlign.center,
-              )
+              ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  children:  <Widget>[
+                    const SizedBox(height: 40),
+                    Image.asset('assets/onboarding_image_2.png'),
+                    const SizedBox(height: 40),
+                    const Text(
+                      "Find the best mortgage deal",
+                      style: TextStyle(
+                          fontFamily: 'Nunito',
+                          fontWeight:  FontWeight.bold,
+                          fontSize: 30.0
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Dari helps you to compare different mortgage offers and allow you to choose the best",
+                      style: TextStyle(
+                        fontFamily: 'Nunito',
+                        color: Colors.grey,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  children:  <Widget>[
+                    const SizedBox(height: 40),
+                    Image.asset('assets/onboarding_image_3.png'),
+                    const SizedBox(height: 40),
+                    const Text(
+                      "Save your time",
+                      style: TextStyle(
+                          fontFamily: 'Nunito',
+                          fontWeight:  FontWeight.bold,
+                          fontSize: 30.0
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 60),
+                    const Text(
+                      "No more branch visits to explore different opportunities. All the deals are now in your hand.",
+                      style: TextStyle(
+                        fontFamily: 'Nunito',
+                        color: Colors.grey,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),),
             ],
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            children:  <Widget>[
-              const SizedBox(height: 40),
-              Image.asset('assets/onboarding_image_2.png'),
-              const SizedBox(height: 40),
-              const Text(
-                "Find the best mortgage deal",
-                style: TextStyle(
-                    fontFamily: 'Nunito',
-                    fontWeight:  FontWeight.bold,
-                    fontSize: 30.0
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 30),
-              const Text(
-                "Dari helps you to compare different mortgage offers and allow you to choose the best",
-                style: TextStyle(
-                  fontFamily: 'Nunito',
-                  color: Colors.grey,
-                  fontSize: 12.0,
-                ),
-                textAlign: TextAlign.center,
-              )
-            ],
-          ),),
-        Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            children:  <Widget>[
-              const SizedBox(height: 40),
-              Image.asset('assets/onboarding_image_3.png'),
-              const SizedBox(height: 40),
-              const Text(
-                "Save your time",
-                style: TextStyle(
-                    fontFamily: 'Nunito',
-                    fontWeight:  FontWeight.bold,
-                    fontSize: 30.0
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 30),
-              const Text(
-                "No more branch visits to explore different opportunities. All the deals are now in your hand.",
-                style: TextStyle(
-                  fontFamily: 'Nunito',
-                  color: Colors.grey,
-                  fontSize: 12.0,
-                ),
-                textAlign: TextAlign.center,
-              )
-            ],
-          ),),
-      ],
+        ],
       ),
       appBar: AppBar(
-        title: const Text("Dari"),
-        backgroundColor: Colors.white,
+          backgroundColor: Colors.white,
+          bottomOpacity: 1.0,
+          elevation: 0.5,
+        title: const Text(
+            "Dari",
+          style: TextStyle(
+              fontFamily: 'Nunito',
+              fontWeight:  FontWeight.bold,
+              fontSize: 30.0
+          ),
+        ),
+        centerTitle: true,
         foregroundColor: const Color(0xFF665BEF),
         actions:  <Widget>[
         Padding(
@@ -119,6 +136,8 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           ),
         ),
         ]
+      ),
+      bottomSheet: PageIndicator(
       ),
     );
   }
